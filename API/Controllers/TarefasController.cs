0f9c6bf838c4d04d17ce59c471c30fac84fda02a
Wesley Santos <wesleysantos.nasci@gmail.com>
2023-04-03 20:21:08 -0300

    Crud Com API

---
+++ API/Controllers/TarefasController.cs
@@ -0,0 +1,74 @@
+﻿using API.Data;
+using API.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [Route("api/[Controller]")]
+    [ApiController]
+    public class TarefasController : ControllerBase
+    {
+        private readonly ITarefaRepository _tarefaRepo;
+        public TarefasController(ITarefaRepository tarefaRepo)
+        {
+            _tarefaRepo = tarefaRepo;
+        }
+
+        [HttpGet]
+        [Route("tarefas")]
+        public async Task<IActionResult> GetTarefas()
+        {
+            var resultado = await _tarefaRepo.GetTarefasAsync();
+            return Ok(resultado);
+        }
+
+        [HttpGet]
+        [Route("tarefa")]
+        public async Task<IActionResult> GetTodoItemByIdAsync(int id)
+        {
+            var tarefa = await _tarefaRepo.GetTarefaByIdAsync(id);
+            return Ok(tarefa);
+        }
+
+        [HttpGet]
+        [Route("tarefascontador")]
+        public async Task<IActionResult> GetTodosAndCountAsync()
+        {
+            var resultado = await _tarefaRepo.GetTarefasEContadorAsync();
+            return Ok(resultado);
+        }
+
+        [HttpPost]
+        [Route("criartarefa")]
+        public async Task<IActionResult> SaveAsync(Tarefa novatarefa)
+        {
+            var resultado = await _tarefaRepo.SaveAsync(novatarefa);
+            return Ok(resultado);
+        }
+
+        [HttpPost]
+        [Route("atualizastatus")]
+        public async Task<IActionResult> UpdateTodosStatusAsync(Tarefa atualizatarefa)
+        {
+            if (atualizatarefa.Id != 0)
+            {
+                var resultado = await _tarefaRepo.UpdateTarefaStatusAsync(atualizatarefa);
+                return Ok(resultado);
+            }
+            return BadRequest("Tarefa não encontrada");
+        }
+
+        [HttpDelete]
+        [Route("deletatarefa")]
+        public async Task<IActionResult> DeleteAsync(int id)
+        {
+            if (id != 0)
+            {
+                var resultado = await _tarefaRepo.DeleteAsync(id);
+                return Ok(resultado);
+            }
+            return BadRequest("Tarefa não encontrada");
+        }
+    }
+}
---
+++ API/Repositories/TarefaRepository.cs
@@ -0,0 +1,93 @@
+﻿using API.Data;
+using Dapper;
+using Microsoft.AspNetCore.Connections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Repositories
+{
+    public class TarefaRepository : ITarefaRepository
+    {
+        private DbSession _db;
+        public TarefaRepository(DbSession dbSession)
+        {
+            _db = dbSession;
+        }
+        public async Task<int> DeleteAsync(int id)
+        {
+            using (var conn = _db.Connection)
+            {
+                string command = @"DELETE FROM Tarefas WHERE Id = @Id";
+                var resultado = await conn.ExecuteAsync(sql: command, param: new { id });
+                return resultado;
+            }
+        }
+
+        public async Task<Tarefa> GetTarefaByIdAsync(int id)
+        {
+            using (var conn = _db.Connection)
+            {
+                var query = "SELECT * FROM Tarefas WHERE Id = @Id";
+                Tarefa tarefa = await conn.QueryFirstOrDefaultAsync<Tarefa>(sql: query, param: new { id });
+                return tarefa;
+            }
+        }
+
+        public async Task<List<Tarefa>> GetTarefasAsync()
+        {
+            using (var coon = _db.Connection)
+            {
+                string query = "SELECT * FROM Tarefas";
+                List<Tarefa> tarefas = (await coon.QueryAsync<Tarefa>(sql: query)).ToList();
+                return tarefas;
+            }
+        }
+
+        public async Task<TarefaContainer> GetTarefasEContadorAsync()
+        {
+            using (var conn = _db.Connection)
+            {
+                string query = @"SELECT COUNT(*) FROM Tarefas
+                                 SELECT * FROM Tarefas";
+                var reader = await conn.QueryMultipleAsync(sql: query);
+                return new TarefaContainer
+                {
+                    Contador = (await reader.ReadAsync<int>()).FirstOrDefault(),
+                    Tarefas = (await reader.ReadAsync<Tarefa>()).ToList()
+                };
+            }
+        }
+
+        public async Task<int> SaveAsync(Tarefa novaTarefa)
+        {
+            using (var conn = _db.Connection)
+            {
+                string command = @"INSERT INTO Tarefas(DESCRICAO, ISCOMPLETA) 
+                                               VALUES(@DESCRICAO, @ISCOMPLETA)";
+                var resultado = await conn.ExecuteAsync(sql: command, param: novaTarefa);
+                return resultado;
+            }
+        }
+
+        public async Task<int> UpdateTarefaStatusAsync(Tarefa atualizaTarefa)
+        {
+            try
+            {
+                using (var conn = _db.Connection)
+                {
+                    string command = @"UPDATE Tarefas SET DESCRICAO = @Descricao, ISCOMPLETA = @IsCompleta
+                                                      WHERE Id = @Id";
+                    var resultado = await conn.ExecuteAsync(command, param: new { atualizaTarefa.Descricao, atualizaTarefa.IsCompleta, atualizaTarefa.Id});
+                    return resultado;
+                }
+
+            }
+            catch (System.Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
